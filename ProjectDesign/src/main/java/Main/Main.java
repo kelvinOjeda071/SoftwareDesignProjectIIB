d44ffc073093f09be3d4fb108c928d6f07d84170
KelvinOjeda <kelvin.ojeda.epn@gmail.com>
2022-01-31 09:51:08 -0500

    Change 2 into branch Example

--- ProjectDesign/src/main/java/Main/Main.java
+++ ProjectDesign/src/main/java/Main/Main.java
@@ -7,7 +7,8 @@ package Main;
 public class Main {
 
     public static void main(String[] args) {
-       
+        System.out.println("Mensaje de prueba");
+        System.out.println("Cambio 1");
     }
     
 }
