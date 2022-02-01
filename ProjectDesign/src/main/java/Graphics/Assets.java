f39d7f4da72ba931b1fc218ae994eaa17aa21cab
KelvinOjeda <kelvin.ojeda.epn@gmail.com>
2022-01-31 21:03:54 -0500

    Change in Assets Class

--- ProjectDesign/src/main/java/Graphics/Assets.java
+++ ProjectDesign/src/main/java/Graphics/Assets.java
@@ -4,12 +4,15 @@
  */
 package Graphics;
 
+import java.awt.image.BufferedImage;
+
 /**
  *
  * @author KelvinOjeda
  */
 public class Assets {
+    public static BufferedImage player;
     public static void init(){
-        
+        player = Loader.ImageLoader("\\ships\\player.png");
     }
 }
