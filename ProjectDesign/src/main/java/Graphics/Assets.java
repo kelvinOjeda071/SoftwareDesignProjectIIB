78fde7d2d9f00596a742c35004d256f418e30c2b
KelvinOjeda <kelvin.ojeda.epn@gmail.com>
2022-01-31 19:48:55 -0500

    Create the folder when we put the resources into the game

---
+++ ProjectDesign/src/main/java/Graphics/Assets.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Graphics;
+
+/**
+ *
+ * @author KelvinOjeda
+ */
+public class Assets {
+    public static void init(){
+        
+    }
+}
