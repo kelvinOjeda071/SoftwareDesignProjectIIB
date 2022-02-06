af6d557d6578275fd1a187f0629c92f20d24c7bf
KelvinOjeda <kelvin.ojeda.epn@gmail.com>
2022-02-06 09:26:12 -0500

    Create method to move the ship around the window

--- ProjectDesignII/src/GameObject/Player.java
+++ ProjectDesignII/src/GameObject/Player.java
@@ -18,20 +18,37 @@ import java.awt.image.BufferedImage;
  */
 public class Player extends MovingObject{
 
-    private Vector2D heading;
+    private Vector2D heading;// The direction vector
+    private Vector2D acceleration; // The variation of velocity with respect to time
+    private final double ACC=0.2; // The scale of acceleration to gt the acceleraion vector
+    private final double DELTAANGLE = 0.1;
     
-    public Player(Vector2D position, Vector2D velocity, BufferedImage texture) {
-        super(position, velocity, texture);
+    public Player(Vector2D position, Vector2D velocity, double maxVelocity, BufferedImage texture) {
+        super(position, velocity, maxVelocity, texture);
         heading= new Vector2D(0,1);
+        acceleration = new Vector2D();
     }
 
     @Override
     public void update() {
         if(KeyBoard.RIGHT)
-            angle +=Math.PI/20;
+            angle += DELTAANGLE;
         if(KeyBoard.LEFT)
-            angle -=Math.PI/20;
+            angle -= DELTAANGLE;
+        if(KeyBoard.UP){
+            acceleration=heading.scale(ACC);
+        }else{
+            if(velocity.getMagnitude()!=0){
+                acceleration=(velocity.scale(-1).normalize()).scale(ACC/2);
+            }
+        }
+        
+        velocity = velocity.add(acceleration); //Acceleration is the variation of the velocity
+        velocity.limit(maxVelocity); //Limited the velocity vector
+        
         heading=heading.setDirection(angle-Math.PI/2);
+        
+        position=position.add(velocity); //Velocity is the variation of the position
     }
 
     @Override
--- ProjectDesignII/src/Math/Vector2D.java
+++ ProjectDesignII/src/Math/Vector2D.java
@@ -9,27 +9,54 @@ package Math;
  * @author KelvinOjeda
  */
 public class Vector2D {
-    
+
     private double x, y;
-    
-    public Vector2D(){
-        x=0;
-        y=0;
+
+    public Vector2D() {
+        x = 0;
+        y = 0;
     }
-    
-    public Vector2D(double x, double y){
-        this.x=x;
-        this.y=y;
+
+    public Vector2D(double x, double y) {
+        this.x = x;
+        this.y = y;
     }
-    
-    public double getMagnitude(){
-        return Math.sqrt(x*x + y*y);
+
+    public double getMagnitude() {
+        return Math.sqrt(x * x + y * y);
     }
-    
-    public Vector2D setDirection(double angle){
+
+    public Vector2D setDirection(double angle) {
         return new Vector2D(
-                Math.cos(angle)*getMagnitude(), 
-                Math.sin(angle)*getMagnitude());
+                Math.cos(angle) * getMagnitude(),
+                Math.sin(angle) * getMagnitude());
+    }
+
+    public Vector2D add(Vector2D v) {
+        return new Vector2D(this.x + v.getX(), this.y + v.getY());
+    }
+
+    public Vector2D scale(double value) { //Multiply by a scale
+        return new Vector2D(this.x * value, this.y * value);
+    }
+
+    public void limit(double value) {
+        if (x > value) {
+            x = value;
+        }
+        if (x < -value) {
+            x = -value;
+        }
+        if (y > value) {
+            y = value;
+        }
+        if (y < -value) {
+            y = -value;
+        }
+    }
+    
+    public Vector2D normalize(){
+        return new Vector2D(x/getMagnitude(), y/getMagnitude());
     }
 
     public double getX() {
@@ -47,6 +74,5 @@ public class Vector2D {
     public void setY(double y) {
         this.y = y;
     }
-    
-    
+
 }
