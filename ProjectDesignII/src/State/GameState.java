24fc0b5012eba37a11a5e03ebaf68f688670134b
Jonathan Puglla <jonathan.puglla@epn.edu.ec>
2022-02-11 16:12:31 -0500

    Game Objects that are moving over the screen are now added to an array List structure in GameState Class.

--- ProjectDesignII/src/State/GameState.java
+++ ProjectDesignII/src/State/GameState.java
@@ -4,31 +4,49 @@
  */
 package State;
 
+import GameObject.Object;
 import GameObject.Player;
 import Graphics.Assets;
 import Math.Vector2D;
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 /**
  *
  * @author KelvinOjeda
  */
 public class GameState {
-    
+    /* Attributes */
     private Player player;
+    private ArrayList<Object> movingObjects = 
+            new ArrayList<Object>();
     
-    
+    /* Constructor */
     public GameState(){
 
         player= new Player(new Vector2D(100,500), new Vector2D(), 7.5, Assets.player);
-
+        movingObjects.add(player);
     }
     
+    /* Methods */
+    
     public void update(){
-        player.update();
+        for(int i = 0; i < movingObjects.size(); i++) {
+            movingObjects.get(i).update();
+        }
     }
     
     public void draw(Graphics g){
-        player.draw(g);
-    } 
+        for(int i = 0; i < movingObjects.size(); i++) {
+            movingObjects.get(i).draw(g);
+        }
+    }
+    
+    /* Get */
+    
+    /* Array List access */
+    public ArrayList<Object> getMovingObjects() {
+        return movingObjects;
+    }
+    
 }
