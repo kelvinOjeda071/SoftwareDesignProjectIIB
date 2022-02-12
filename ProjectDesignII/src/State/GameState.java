f4a495b8a8017bef1bd117e8d4dc36c13fa7a86e
Jonathan Puglla <jonathan.puglla@epn.edu.ec>
2022-02-11 22:15:59 -0500

    Meteors are now on screen. Class Asteroid allows to add the meteors on the game. Minor CODE updates over the UPDATE method.

--- ProjectDesignII/src/State/GameState.java
+++ ProjectDesignII/src/State/GameState.java
@@ -4,15 +4,18 @@
  */
 package State;
 
+import GameObjects.Asteroid;
 import GameObjects.Object;
 import GameObjects.Player;
-import Graphics.Assets;
+import Graphics.Asset;
 import Math.Vector2D;
 import java.awt.Graphics;
 import java.util.ArrayList;
-import GameObjects.Constants;
+import GameObjects.Constant;
+import GameObjects.Size;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
 /**
  *
  * @author KelvinOjeda
@@ -21,6 +24,7 @@ public class GameState {
     /* Attributes */
     private Player player;
     private ArrayList<Object> movingObjects = new ArrayList<Object>();
+    private int meteors;
     
     /* Constructor */
     public GameState(){
@@ -30,27 +34,85 @@ public class GameState {
         int hPos = 0;
         
         /* Calculates the vector x and y attributes */
-        wPos = Constants.WIDTH / 2 - Assets.player.getWidth() / 2;
-        hPos = Constants.HEIGHT / 2 - Assets.player.getHeight() / 2;
+        wPos = Constant.WIDTH / 2 - Asset.player.getWidth() / 2;
+        hPos = Constant.HEIGHT / 2 - Asset.player.getHeight() / 2;
         
         player= new Player(
             new Vector2D(wPos, hPos), 
             new Vector2D(), 
-            Constants.PLAYER_MAX_VEL, 
-            Assets.player, this
+            Constant.PLAYER_MAX_VEL, 
+            Asset.player, this
         );
         
         /* Adds the player as a moving object */
         movingObjects.add(player);
+        
+        /* Meteors quantity */
+        meteors = 1;
+        
+        /* Starts the asteroids wave */
+        startWave();
     }
     
     /* Methods */
+    
+    /* Deploys the asteroids */
+    private void startWave(){
+        /* Asteroid position */
+        double x;
+        double y;
+        double myAngle;
+        
+        /* Counter */
+        int i = 0;
+        
+        /* Starts the wave*/
+        for(i = 0; i < meteors; i++){
+            x = i % 2 == 0 ? Math.random() * Constant.WIDTH : 0;
+            y = i % 2 == 0 ? 0 : Math.random() * Constant.WIDTH;
+            
+            /* Texture */
+            BufferedImage texture = 
+                Asset.bigs[
+                    (int) (Math.random() * Asset.bigs.length)
+                ];
+            
+            /* Calculates the angle*/
+            myAngle = Math.random() * Math.PI * 2;
+            
+            /* Adds the asteroid as a moving object */
+            movingObjects.add(
+                    new Asteroid(
+                        new Vector2D(x, y),
+                        new Vector2D(0, 1).setDirection(myAngle),
+                        Constant.METEOR_VEL * Math.random() + 1,
+                        texture, 
+                        this, 
+                        Size.BIG
+                    )
+            );
+        }
+        
+        /* Increases game difficulty */
+        meteors ++;
+        
+    }
     
     /* Updates the object status */
     public void update(){
         for(int i = 0; i < movingObjects.size(); i++) {
             movingObjects.get(i).update();
         }
+        
+        /* Decides if a new wave is necessary */
+        for(int i = 0; i < movingObjects.size(); i++) {
+            if(movingObjects.get(i) instanceof Asteroid){
+               return; 
+            }
+        }
+        
+        /* Starts the new wave */
+        startWave();
     }
     
     /* Draws the desired object */
