fc3d8d5be6295b38042c301b72ac018aba7d91b0
Jonathan Puglla <jonathan.puglla@epn.edu.ec>
2022-02-11 22:44:41 -0500

    Objects can now collide. Collide method is added to player and laser UPDATE method.

--- ProjectDesignII/src/GameObjects/Laser.java
+++ ProjectDesignII/src/GameObjects/Laser.java
@@ -15,61 +15,72 @@ import java.awt.image.BufferedImage;
  *
  * @author Jonathan Puglla
  */
-public class Laser extends Object{
-    
+public class Laser extends Object {
+
     /* Constructor */
     public Laser(
-            Vector2D position, 
-            Vector2D velocity, 
-            double maxVelocity, 
-            double angle, 
-            BufferedImage texture, 
+            Vector2D position,
+            Vector2D velocity,
+            double maxVelocity,
+            double angle,
+            BufferedImage texture,
             GameState gameState
     ) {
         /* Parent attributes */
         super(position, velocity, maxVelocity, texture, gameState);
-        
+
         /* Sets the attributes of the constructor */
         this.angle = angle;
         this.velocity = velocity.scale(maxVelocity);
     }
-    
+
     /* Methods */
-    
+ /* Obtains the center of the Laser */
+    @Override
+    protected Vector2D getCenter() {
+        /* Local attributes */
+        double posX = position.getX() + width / 2;
+        double posY = position.getY() + width / 2;
+
+        /* Returns the center */
+        return new Vector2D(posX, posY);
+    }
+
     /* Update the position */
     @Override
     public void update() {
         /* Attributes */
         position = position.add(velocity);
-      
+
         /* Deletes the laser */
-        if
-        (     
-               position.getX() < 0 
-            || position.getY() < 0 
-            || position.getX() > Constants.WIDTH 
-            || position.getY() > Constants.HEIGHT
-        ) {
+        if (position.getX() < 0
+                || position.getY() < 0
+                || position.getX() > Constant.WIDTH
+                || position.getY() > Constant.HEIGHT) {
+            
             /* Removes the laser */
-            gameState.getMovingObjects().remove(this);
+            Destroy();
         }
+
+        /* Detects a collition */
+        collidesWith();
     }
-    
+
     /* Draws the object */
     @Override
     public void draw(Graphics g) {
         /* Graphics attribute */
-        Graphics2D g2d = (Graphics2D)g;
-        
+        Graphics2D g2d = (Graphics2D) g;
+
         /* Position transformation*/
         at = AffineTransform.getTranslateInstance(
-                position.getX() - width / 2, 
+                position.getX() - width / 2,
                 position.getY()
         );
-        
+
         /* Rotation */
         at.rotate(angle, width / 2, 0);
-        
+
         /* Draws the texture */
         g2d.drawImage(texture, at, null);
     }
